eadb94f809005aee0c14024e11d39c2d85082c41
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-08-16 21:03:07 +0300

    add root component and initial routing

---
+++ shared/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div id="app-view">
+        <h1>Todos</h1>
+
+        <hr />
+
+        {this.props.children}
+      </div>
+    );
+  }
+}
