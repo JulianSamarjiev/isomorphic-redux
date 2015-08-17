ce17cd74616abc944de08a9cadd200f020226238
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-08-17 20:19:48 +0300

    typo fixed and adjust files to tutorial code

--- server.jsx
+++ server.jsx
@@ -1,23 +1,41 @@
 import express from 'express';
+import React from 'react';
+import { Router } from 'react-router';
+import Location from 'react-router/lib/Location';
+import routes from 'routes';
 
 const app = express();
 
 app.use((req, res) => {
-  const HTML = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title>Isomorphic Redux Demo</title>
-    </head>
-    <body>
-      <div id="react-view"></div>
-      <script type="application/javascript" src="/bundle.js"></script>
-    </body>
-  </html>
-  `;
+  const location = new Location(req.path, req.query);
 
-  res.end(HTML);
+  Router.run(routes, location, (err, routeState) => {
+    if (err) return console.error(err);
+
+    if (!routeState) return res.status(404).end('404');
+
+    const InitialComponent = (
+      <Router {...routeState} />
+    );
+
+    const componentHTML = React.renderToString(InitialComponent);
+
+    const HTML = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Isomorphic Redux Demo</title>
+      </head>
+      <body>
+        <div id="react-view"></div>
+        <script type="application/javascript" src="/bundle.js"></script>
+      </body>
+    </html>
+    `;
+
+    res.end(HTML);
+  });
 });
 
 export default app;
--- shared/components/index.jsx
+++ shared/components/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class App extends React.Component {
+export default class AppView extends React.Component {
   render() {
     return (
       <div id="app-view">
