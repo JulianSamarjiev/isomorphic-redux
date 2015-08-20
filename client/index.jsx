2e3468b3b28d9b5de4fd34cd0b9c4bb60f1174a7
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-08-20 15:17:58 +0300

    initialize Redux on client

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,30 @@
-import React        from 'react';
-import { Router }   from 'react-router';
-import { history }  from 'react-router/lib/BrowserHistory';
-import routes       from 'routes';
+import React                            from 'react';
+import { Router }                       from 'react-router';
+import { history }                      from 'react-router/lib/BrowserHistory';
+import routes                           from 'routes';
+import { createStore, combineReducers } from 'redux';
+import { Provider }                     from 'react-redux';
+import * as reducers                    from 'reducers';
+import { fromJS }                       from 'immutable';
+
+let initialState = window.__INITIAL_STATE__;
+
+// Transform into Imutable.js collections,
+// but leave top level keys untouched for Redux
+Object
+  .keys(initialState)
+  .forEach(key => {
+    initialState[key] = fromJS(initialState[key]);
+  });
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer, initialState);
 
 React.render(
-  <Router children={routes} history={history} />,
-  document.getElementById('react-view')
+  <Provider store={store}>
+    {() =>
+      <Router children={routes} history={history} />
+    }
+  </Provider>
+  document.getElementById('react-view');
 );
