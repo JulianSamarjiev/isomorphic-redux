524c7ce77a1b2e48cf173d835863579b5472de79
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-08-22 17:34:30 +0300

    add asynchronous actions

--- shared/actions/TodoActions.js
+++ shared/actions/TodoActions.js
@@ -1,8 +1,11 @@
+import request from 'axios';
+
+const BACKEND_URL = 'https://webtask.it.auth0.com/api/run/wt-milomord-gmail_com-0/redux-tutorial-backend?webtask_no_cache=1';
+
 export function createTodo(text) {
   return {
     type: 'CREATE_TODO',
-    text,
-    date: Date.now()
+    promise: request.post(BACKEND_URL, { text })
   }
 }
 
--- shared/reducers/TodoReducer.js
+++ shared/reducers/TodoReducer.js
@@ -5,7 +5,7 @@ const defaultState = new Immutable.List();
 export default function todoReducer(state = defaultState, action) {
   switch(action.type) {
     case 'CREATE_TODO':
-      return state.concat(action.text);
+      return state.concat(action.res.data.text);
     case 'EDIT_TODO':
       return state.set(action.id, action.text);
     case 'DELETE_TODO':
